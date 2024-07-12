a2575f9166ee7a59743ccd1165c93840c35f41ff
phamquangviet891 <82923647+VietPam@users.noreply.github.com>
2024-07-12 13:21:30 +0700

    feat: add attendance components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React from "react";
+import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import DefaultLayout from "./layouts/MainLayout/DefaultLayout";
+import routes from "./router";
+
 function App() {
   return (
-    <div className="App">
-      
-      hello
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {routes.map(route => {
+            let Layout = DefaultLayout
+            if (route.layout) {
+              Layout = route.layout
+            }
+            else if (route.layout === null) {
+              Layout = Fragment
+            }
+            const Page = route.page
+            return <Route path={route.path} element={
+              <Layout>
+                <Page></Page>
+              </Layout>
+            } />
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
